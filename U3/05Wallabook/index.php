82154d5b9be33208b37c2016fe4a6fe8f1e18f46
rmmateosv <rmmateosv@gmail.com>
2025-11-06 08:40:32 +0000

    1106

--- U3/05Wallabook/index.php
+++ U3/05Wallabook/index.php
@@ -10,6 +10,19 @@ require_once 'controlador.php';
     <title>Document</title>
 </head>
 <body>
-    
+    <div>
+        <?php
+        if (isset($error)) {
+            echo '<h3 style="color:red;">' . $error . '</h3>';
+        }
+        ?>
+    </div>
+    <div>
+        <?php
+        if (isset($mensaje)) {
+            echo '<h3 style="color:green;">' . $mensaje . '</h3>';
+        }
+        ?>
+    </div>    
 </body>
 </html>
---
+++ U3/05Wallabook/wallabook.sql
@@ -0,0 +1,37 @@
+drop database if exists wallabook;
+create database wallabook;
+use wallabook;
+
+create table usuarios(
+	id int primary key auto_increment,
+	email varchar(255) unique not null,
+    ps blob not null,
+    nombre varchar(255) not null
+)engine innodb;
+
+create table libros(
+	id int primary key auto_increment,
+    fechaC datetime not null,
+    isbn varchar(100) not null,
+    titulo varchar(100) not null,
+    autor varchar(100) not null,
+    descripcion varchar(255) not null,
+    carpetaS3Fotos varchar(255),
+    estado enum ('Disponible', 'Reservado', 'Vendido') not null default 'Disponible',
+    precio float not null,
+    vendedor int not null,
+    comprador int null,
+    foreign key (vendedor) references usuarios(id) on update cascade on delete restrict,
+    foreign key (comprador) references usuarios(id) on update cascade on delete restrict
+)engine innodb;
+
+create table mensajes(
+	id int primary key auto_increment,
+    fechaC datetime not null,
+    texto varchar(255) not null,
+    emisor int not null, 
+    libro int not null,
+    foreign key (emisor) references usuarios(id) on update cascade on delete restrict,
+    foreign key (libro) references libros(id) on update cascade on delete restrict
+)
+
