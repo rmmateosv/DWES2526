11693c7f55b768fc5bb160029a29eedcc7683f93
rmmateosv <rmmateosv@gmail.com>
2025-12-19 10:51:37 +0000

    1912

--- U4/proyecto1/database/migrations/2025_12_18_091423_create_alumnos_table.php
+++ U4/proyecto1/database/migrations/2025_12_18_091423_create_alumnos_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('alumnos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nombre')->nullable(false);
+            $table->string('curso')->nullable(false);
         });
     }
 
