4cf465c6d31026f951ac9231969775d297f18131
rmmateosv <rmmateosv@gmail.com>
2026-01-15 09:14:44 +0000

    0115

--- U4/bocadillos/resources/views/vistaProductos.blade.php
+++ U4/bocadillos/resources/views/vistaProductos.blade.php
@@ -7,6 +7,33 @@
     <title>Document</title>
 </head>
 <body>
-    <h2>Selecciona producto</h2>
+    <h2>Nombre del Empleado</h2>
+    <div style="display: flex; flex-direction: row">
+        <div>
+            <h2>Selecciona producto</h2>
+            <table>
+                <tr>
+                    <td>Nombre</td>
+                    <td>Foto</td>
+                    <td>Acción</td>
+                </tr>
+                @foreach ($productos as $p)
+                    <tr>
+                        <td>{{$p->nombre}}</td>
+                        <td><img src="{{ ($p->foto==null?asset('img/generica.png'):asset($p->foto)) }}" alt="producto"></td>
+                        <td>
+                            <button type="submit" name="anadir" value="{{$p->id}}">+</button>
+                            <button type="submit" name="eliminar" value="{{$p->id}}">-</button>
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+
+        <div>
+
+        </div>
+    </div>
+    
 </body>
 </html>
--- U4/bocadillos/routes/web.php
+++ U4/bocadillos/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProductoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('verProductos');
 });
 
 Route::controller(ProductoController::class)->group(function(){
