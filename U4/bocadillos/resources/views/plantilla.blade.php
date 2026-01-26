94beac15de62c6a4417fe4ec4fb2b52c85d47c4d
rmmateosv <rmmateosv@gmail.com>
2026-01-26 23:13:34 +0000

    0126

--- U4/bocadillos/resources/views/plantilla.blade.php
+++ U4/bocadillos/resources/views/plantilla.blade.php
@@ -5,28 +5,50 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bacadillos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div>
-        <a href="">Ventas</a>
-        <a href="">Pedidos</a>
-        <a href="">Productos</a>
-    </div>
-    <h2>Nombre del Empleado</h2>
-    <div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="navbar-brand"  href="">Ventas</a>
+                </li>
+                <li class="nav-item">
+                <a class="navbar-brand"  href="">Pedidos</a>
+                </li>
+                <li class="nav-item">
+                <a class="navbar-brand"  href="">Productos</a>
+                </li>
+            </ul>
+            <span class="navbar-text">Nombre del Empleado</span>
+            <nav class="navbar bg-body-tertiary">
+                <form class="container-fluid justify-content-start">
+                    <button class="btn btn-sm btn-outline-secondary" type="button">Salir</button>
+                </form>
+            </nav>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid">
         @yield('titulo')
+        <p></p>
         @yield('contenido')
-    </div>
-    <div>
-        @if (session('mensaje'))
-           <p style="color:red">{{session('mensaje')}}</p> 
-        @endif
-        @if ($errors->any())
-            @foreach ($errors->all() as $e)
-                <p style="color:red">{{$e}}</p> 
-            @endforeach
-        @endif
-
+       
+        <div class="row">
+            <div class="col">
+                @if (session('mensaje'))
+                    <div class="p-3 text-success-emphasis bg-success-subtle border border-success-subtle rounded-3">{{session('mensaje')}}</div> 
+                @endif
+                @if ($errors->any())
+                    @foreach ($errors->all() as $e)
+                        <div class="p-3 text-danger-emphasis bg-danger-subtle border border-danger-subtle rounded-3">{{$e}}</div> 
+                    @endforeach
+                @endif
+            </div>
+        </div>
     </div>
 </body>
 </html>
--- U4/bocadillos/resources/views/vistaProductos.blade.php
+++ U4/bocadillos/resources/views/vistaProductos.blade.php
@@ -2,80 +2,76 @@
 
 
 @section('titulo')
-    <h3>Venta de bocadillos</h3>
+    <h6 class="h6">Venta de bocadillos</h3>
 @endsection
 
 @section('contenido')
-<div style="display: flex; flex-direction: row">
-    <div style="border:1px solid red;margin:20px;">
-        <h2>Selecciona producto</h2>
-        <table>
-            <tr>
-                <td>Nombre</td>
-                <td>Foto</td>
-                <td>Acción</td>
-            </tr>
-            @foreach ($productos as $p)
-                <tr>
-                    <td>{{$p->nombre}}</td>
-                    <td><img src="{{ ($p->foto==null?asset('img/generica.png'):asset($p->foto)) }}" alt="producto"></td>
-                    <td>
-                        @if (session('pedido'))                            
-                        <form action="{{route('insertarD')}}" method="post">
-                            @csrf
-                            <button type="submit" name="anadir" value="{{$p->id}}">+</button>
-                        </form>
-                        <form action="{{route('eliminarD')}}" method="post">
-                            @csrf
-                            <button type="submit" name="eliminar" value="{{$p->id}}">-</button>
-                        </form>
-                        @endif
-                    </td>
-                </tr>
-            @endforeach
-        </table>
-    </div>
 
-    <div style="border:1px solid red;margin:20px">       
-            @if (session('pedido'))
-                <h3>Pedido:{{session('pedido')->id}} Total:{{session('pedido')->total()}}</h3>
-                <table border="1">
+    <div class="row">
+        <div class="col">
+            <table class="table">
+                <tr>
+                    <td>Nombre</td>
+                    <td>Foto</td>
+                    <td>Acción</td>
+                </tr>
+                @foreach ($productos as $p)
                     <tr>
-                        <td>Producto</td>
-                        <td>Imagen</td>
-                        <td>Cantidad</td>
-                        <td>Precio</td>
-                        <td>Total</td>
-                    </tr>
-                @foreach (session('pedido')->detalles() as $d)
-                    <tr>
-                        <td>{{$d->producto->nombre}}</td>
+                        <td>{{$p->nombre}}</td>
+                        <td><img src="{{ ($p->foto==null?asset('img/generica.png'):asset($p->foto)) }}" alt="producto"></td>
                         <td>
-                        <img src="{{ ($d->producto->foto==null?asset('img/generica.png'):
-                        asset($d->producto->foto)) }}" alt="producto"></td>
-                        <td>{{$d->cantidad}}</td>
-                        <td>{{$d->precio}}</td>
-                        <td>{{($d->cantidad*$d->precio)}}</td>
+                            @if (session('pedido'))                            
+                            <form action="{{route('insertarD')}}" method="post">
+                                @csrf
+                                <button type="submit" name="anadir" value="{{$p->id}}">+</button>
+                            </form>
+                            <form action="{{route('eliminarD')}}" method="post">
+                                @csrf
+                                <button type="submit" name="eliminar" value="{{$p->id}}">-</button>
+                            </form>
+                            @endif
+                        </td>
                     </tr>
                 @endforeach
-                </table>
-    
-                <form action="{{route('modificar',session('pedido')->id)}}" method="post">
-                    @csrf
-                    @method('PUT')
-                    <button type="submit" name="fin">Finalizar</button>
-                    <button type="submit"  name="cancelar">Cancelar</button>
-                </form>
+            </table>
+        </div>
+        <div class="col">       
+                @if (session('pedido'))
+                    <h3>Pedido:{{session('pedido')->id}} Total:{{session('pedido')->total()}}</h3>
+                    <table class="table">
+                        <tr>
+                            <td>Producto</td>
+                            <td>Imagen</td>
+                            <td>Cantidad</td>
+                            <td>Precio</td>
+                            <td>Total</td>
+                        </tr>
+                    @foreach (session('pedido')->detalles() as $d)
+                        <tr>
+                            <td>{{$d->producto->nombre}}</td>
+                            <td>
+                            <img src="{{ ($d->producto->foto==null?asset('img/generica.png'):
+                            asset($d->producto->foto)) }}" alt="producto"></td>
+                            <td>{{$d->cantidad}}</td>
+                            <td>{{$d->precio}}</td>
+                            <td>{{($d->cantidad*$d->precio)}}</td>
+                        </tr>
+                    @endforeach
+                    </table>
+        
+                    <form action="{{route('modificar',session('pedido')->id)}}" method="post">
+                        @csrf
+                        @method('PUT')
+                        <button type="submit" name="fin">Finalizar</button>
+                        <button type="submit"  name="cancelar">Cancelar</button>
+                    </form>
 
-            @else
-                <form action="{{route('crearPedido')}}" method="post">
-                     @csrf
-                    <button>Nuevo Pedido</button>
-                </form>
-            @endif
-            
-  
+                @else
+                    <form action="{{route('crearPedido')}}" method="post">
+                        @csrf
+                        <button>Nuevo Pedido</button>
+                    </form>
+                @endif    
+        </div>
     </div>
-</div>
-
 @endsection
